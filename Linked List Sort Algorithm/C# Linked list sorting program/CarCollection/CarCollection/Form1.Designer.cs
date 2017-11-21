4e6c19c21d3815c28c0e30052094e744b6b2560a
Ali Albayrak <33533989+Hodkan@users.noreply.github.com>
2017-11-22 00:21:51 +0800

    Add files via upload

---
+++ Linked List Sort Algorithm/C# Linked list sorting program/CarCollection/CarCollection/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace CarCollection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd_First = new System.Windows.Forms.Button();
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.textBoxCarName = new System.Windows.Forms.TextBox();
+            this.buttonAdd_Last = new System.Windows.Forms.Button();
+            this.buttonAdd_Before = new System.Windows.Forms.Button();
+            this.buttonAdd_After = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxNodePosition = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNumOfNodes = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd_First
+            // 
+            this.buttonAdd_First.Location = new System.Drawing.Point(182, 69);
+            this.buttonAdd_First.Name = "buttonAdd_First";
+            this.buttonAdd_First.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd_First.TabIndex = 0;
+            this.buttonAdd_First.Text = "Add First";
+            this.buttonAdd_First.UseVisualStyleBackColor = true;
+            this.buttonAdd_First.Click += new System.EventHandler(this.buttonAdd_First_Click);
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.Location = new System.Drawing.Point(12, 38);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(120, 95);
+            this.listBoxOutput.TabIndex = 1;
+            // 
+            // textBoxCarName
+            // 
+            this.textBoxCarName.Location = new System.Drawing.Point(171, 38);
+            this.textBoxCarName.Name = "textBoxCarName";
+            this.textBoxCarName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCarName.TabIndex = 2;
+            // 
+            // buttonAdd_Last
+            // 
+            this.buttonAdd_Last.Location = new System.Drawing.Point(182, 98);
+            this.buttonAdd_Last.Name = "buttonAdd_Last";
+            this.buttonAdd_Last.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd_Last.TabIndex = 3;
+            this.buttonAdd_Last.Text = "Add Last";
+            this.buttonAdd_Last.UseVisualStyleBackColor = true;
+            this.buttonAdd_Last.Click += new System.EventHandler(this.buttonAdd_Last_Click);
+            // 
+            // buttonAdd_Before
+            // 
+            this.buttonAdd_Before.Location = new System.Drawing.Point(372, 69);
+            this.buttonAdd_Before.Name = "buttonAdd_Before";
+            this.buttonAdd_Before.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd_Before.TabIndex = 4;
+            this.buttonAdd_Before.Text = "Add Before";
+            this.buttonAdd_Before.UseVisualStyleBackColor = true;
+            this.buttonAdd_Before.Click += new System.EventHandler(this.buttonAdd_Before_Click);
+            // 
+            // buttonAdd_After
+            // 
+            this.buttonAdd_After.Location = new System.Drawing.Point(372, 98);
+            this.buttonAdd_After.Name = "buttonAdd_After";
+            this.buttonAdd_After.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd_After.TabIndex = 5;
+            this.buttonAdd_After.Text = "Add After";
+            this.buttonAdd_After.UseVisualStyleBackColor = true;
+            this.buttonAdd_After.Click += new System.EventHandler(this.buttonAdd_After_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(29, 155);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 6;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(29, 188);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxNodePosition
+            // 
+            this.textBoxNodePosition.Location = new System.Drawing.Point(358, 38);
+            this.textBoxNodePosition.Name = "textBoxNodePosition";
+            this.textBoxNodePosition.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNodePosition.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Car Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Node Position";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(355, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Number Of Nodes";
+            // 
+            // textBoxNumOfNodes
+            // 
+            this.textBoxNumOfNodes.Enabled = false;
+            this.textBoxNumOfNodes.Location = new System.Drawing.Point(358, 205);
+            this.textBoxNumOfNodes.Name = "textBoxNumOfNodes";
+            this.textBoxNumOfNodes.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumOfNodes.TabIndex = 12;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(182, 128);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 13;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(29, 218);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 14;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(182, 218);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 15;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 261);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxNumOfNodes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNodePosition);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd_After);
+            this.Controls.Add(this.buttonAdd_Before);
+            this.Controls.Add(this.buttonAdd_Last);
+            this.Controls.Add(this.textBoxCarName);
+            this.Controls.Add(this.listBoxOutput);
+            this.Controls.Add(this.buttonAdd_First);
+            this.Name = "Form1";
+            this.Text = "Linked List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd_First;
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.TextBox textBoxCarName;
+        private System.Windows.Forms.Button buttonAdd_Last;
+        private System.Windows.Forms.Button buttonAdd_Before;
+        private System.Windows.Forms.Button buttonAdd_After;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxNodePosition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNumOfNodes;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonLoad;
+    }
+}
+
---
+++ Linked List Sort Algorithm/C# Linked list sorting program/CarCollection/CarCollection/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Name: Ali Albayrak 
+//Student ID - P304320 
+//  Portfolio Activity 1.3
+// 08/09/13
+/* In the previous portfolio activity AT1.1 you added a find button and error trapping, now you will
+need to add a sort button.There is no sort option for Linked Lists, so you will need to research a
+suitable sort algorithm. Once you have selected a sort algorithm you will need to complete the
+following tasks;
+ Create a document to save the following items,
+o Pseudo code for the sort algorithm,
+o References to this code,
+o A discuss on the benefits and pit falls of the algorithm,
+o The computational complexity of the algorithm.
+ Add a new button to Sort the items in the Linked List(ascending).
+ Test the sort button on your UI form using more than 10 items.
+ Record your testing session with a series of screen captures.
+ Add comments to all the code (not XML)*/
+//                                                                 Portfolio Activity 1.3 : Sorting
+
+namespace CarCollection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        LinkedList<string> myCarCollection = new LinkedList<string>();
+
+
+        //Show Method : Clears the input fields and displays number of nodes and  every car in the myCarCollection linkedlist 
+        private void showLinkedList()
+        {
+            // clear input boxes and list box
+            listBoxOutput.Items.Clear();
+            textBoxCarName.Clear();
+            textBoxNodePosition.Clear();
+            // display number of list nodes
+            textBoxNumOfNodes.Text = numberOfNodes().ToString();
+            // display linked list
+            foreach (string car in myCarCollection)
+                listBoxOutput.Items.Add(car);
+        }
+
+        //returns the number of nodes
+        private int numberOfNodes()
+        {
+            return myCarCollection.Count;
+        }
+
+
+
+        // Add first button : adds the car name (taken from textBoxCarName text box) into first place of collection
+        private void buttonAdd_First_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxCarName.Text))
+            {
+                myCarCollection.AddFirst(textBoxCarName.Text);
+                showLinkedList();
+            }
+            else
+                MessageBox.Show("Please enter a Car name");
+
+        }
+
+        //add last button : adds the car name (taken from textBoxCarName text box) into last place of collection
+        private void buttonAdd_Last_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxCarName.Text))
+            {
+                myCarCollection.AddLast(textBoxCarName.Text);
+                showLinkedList();
+            }
+            else
+                MessageBox.Show("Please enter a Car name");
+
+        }
+
+        //Add before button : adds the car name (taken from textBoxCarName text box) into collection before the node position
+        private void buttonAdd_Before_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxCarName.Text) &&
+                        (!string.IsNullOrEmpty(textBoxNodePosition.Text)))
+
+            {
+                try
+                {
+                    int nodePos = Convert.ToInt32(textBoxNodePosition.Text);
+                    string x = myCarCollection.ElementAt(nodePos);
+                    LinkedListNode<string> current = myCarCollection.Find(x);
+                    myCarCollection.AddBefore(current, textBoxCarName.Text);
+                    showLinkedList();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Please enter valid node number (Node Position cannot be greater than " + ((myCarCollection.Count) - 1) + " )");  // List index starts with 0 that is why "-1"
+
+                }
+            }
+            else
+                MessageBox.Show("Please enter a Car name and Node position");
+
+        }
+
+        //Add after button : adds the car name (taken from textBoxCarName text box) into collection after the node position
+        private void buttonAdd_After_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxCarName.Text) &&
+                    (!string.IsNullOrEmpty(textBoxNodePosition.Text)))
+            {
+                try
+                {
+                    int nodePos = Convert.ToInt32(textBoxNodePosition.Text);
+                    string x = myCarCollection.ElementAt(nodePos);
+                    LinkedListNode<string> current = myCarCollection.Find(x);
+                    myCarCollection.AddAfter(current, textBoxCarName.Text);
+                    showLinkedList();
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Please enter valid node number (Node Position cannot be greater than " + ((myCarCollection.Count)-1) + " )");          // List index starts with 0 that is why "-1"
+
+                }
+                
+            }
+            else
+                MessageBox.Show("Please enter a Car name and Node position");
+
+        }
+
+        //Remove button : Removes the car that matches with textboxCarName.text from the collection
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxCarName.Text))
+            {
+                myCarCollection.Remove(textBoxCarName.Text);
+                showLinkedList();
+            }
+            else
+                MessageBox.Show("Please enter a Car name");
+
+        }
+
+
+        //Clear Button : Clears the collection 
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            myCarCollection.Clear();
+            showLinkedList();
+
+        }
+
+        //Search button : Searchs for the text of the textBoxCarName in the myCarCollection
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string current = myCarCollection.Find((textBoxCarName.Text)).Value;         
+                int a = listBoxOutput.Items.IndexOf(current);
+                listBoxOutput.SetSelected(a, true);                                         // Finds the element and sets it selected on the listbox
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Car could not found.");
+
+            }
+        }
+
+
+        //Sorts the myCarCollection linked list alphabetically by using Insertion sort algorithm.
+        private void buttonSort_Click(object sender, EventArgs e)               
+        {
+            listBoxOutput.Items.Clear();
+            string temp;
+            int j;
+            int i = 1;
+            while (i < myCarCollection.Count)
+            {
+                j = i;
+                while(j>0 && string.Compare(myCarCollection.ElementAt(j-1), myCarCollection.ElementAt(j)) > 0)
+                {
+                    LinkedListNode<string> current = myCarCollection.Find(myCarCollection.ElementAt(j-1));
+                    temp = current.Next.Value;
+                    myCarCollection.Find(myCarCollection.ElementAt(j)).Value = myCarCollection.Find(myCarCollection.ElementAt(j-1)).Value;
+                    myCarCollection.Find(myCarCollection.ElementAt(j-1)).Value = temp;
+                    j = j - 1;
+                }
+                i = i + 1;
+            }            
+            foreach (string car in myCarCollection)
+                listBoxOutput.Items.Add(car);         
+        }
+
+
+        // Load button to load cars from cars.txt file by calling the readTxtFile method
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                readTextFile();
+                showLinkedList();
+            }
+            catch
+            {
+                MessageBox.Show("Cars text cannot found. Please check your txt file.");
+            }
+        }
+
+
+        // Method to open a stream reader and lines of
+        // text into the "cars.txt" file from c:\temp\ path
+        private void readTextFile()
+        {
+            string line = "";
+            StreamReader file = new StreamReader(@"c:\temp\cars.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                myCarCollection.AddFirst(line);
+            }
+            file.Close();
+        }
+    }
+}
